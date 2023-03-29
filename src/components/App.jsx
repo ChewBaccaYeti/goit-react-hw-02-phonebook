a41b5988651dfb3a20a5eadc7b6a40266cbea24a
Venedykt <chewbaccayeti@gmail.com>
2023-03-29 21:20:49 +0300

    Packages and project structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import {} from '';
+import {} from '';
+import {} from '';
+import {} from '';
+import {} from '';
+import {} from '';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Van DarkHolme', number: '459-12-56' },
+      { id: 'id-2', name: 'Johnson Lebovski', number: '443-89-12' },
+      { id: 'id-3', name: 'Billy Herrington', number: '645-17-79' },
+      { id: 'id-4', name: 'Gena Ciderusni', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+}
